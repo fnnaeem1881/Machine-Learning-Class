33772c08018e2e57021dac0653e1fffc43d6e21f
Mehedi Hasan <mehedinaeem66@gmail.com>
2025-10-23 17:03:33 +0600

    loop with break and continue

--- loop.py
+++ loop.py
@@ -1,31 +1,44 @@
-for i in range(1, 11):
+# for i in range(1, 11):
+#     print(i, end=" ")
+# print("\n")
+
+# fruits = ["Apple", "Banana", "Orange", "Mango"]
+# for fruit in fruits:
+#     print(fruit)
+# print()
+
+# text = "Mehedi Hasan"
+# for char in text:
+#     print(char)
+# print()
+
+# for i in range(2, 21, 2):
+#     print(i, end=" ")
+# print("\n")
+
+
+# w = 1
+# while w <= 5:
+#     print(w)
+#     w += 1
+# print()
+
+
+# j = 5
+# while j > 0:
+#     print(j)
+#     j -= 1
+# print()
+
+
+for i in range(1, 10):
+    if i == 5:
+        break
     print(i, end=" ")
 print("\n")
 
-fruits = ["Apple", "Banana", "Orange", "Mango"]
-for fruit in fruits:
-    print(fruit)
-print()
-
-text = "Mehedi Hasan"
-for char in text:
-    print(char)
-print()
-
-for i in range(2, 21, 2):
+for i in range(1, 10):
+    if i == 5:
+        continue
     print(i, end=" ")
 print("\n")
-
-
-w = 1
-while w <= 5:
-    print(w)
-    w += 1
-print()
-
-
-j = 5
-while j > 0:
-    print(j)
-    j -= 1
-print()
