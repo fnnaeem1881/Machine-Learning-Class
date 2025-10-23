43f3942b3d9b617f019c6250b76883557344cd0b
Mehedi Hasan <mehedinaeem66@gmail.com>
2025-10-23 16:57:34 +0600

    even odd check

--- basic.py
+++ basic.py
@@ -2,3 +2,10 @@
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 print("Sum is:", a + b)
+
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
